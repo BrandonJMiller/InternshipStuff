4e6834f2ba587853a0ac9d1613d160d606752d79
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-03-04 22:17:47 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 /*
 .get('/', (req, res) => {
 	res.send('Hello World')
 })
-*/
+
 .listen(port, () => {
 	console.log(`listening on ${PORT}`)
 })
-
+*/

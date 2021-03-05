8fb6aae956a01a3b4c43cf576716a51a584a7f94
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-03-04 20:47:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+/*const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
@@ -9,3 +20,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
+*/

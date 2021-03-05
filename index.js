e088f63c2e0cf8465d55f62c6d5970846b0fda16
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-03-04 22:25:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-app.get('/', function(req,res) {
-	res.redirect('/todo');
-});
-
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 /*
 .get('/', (req, res) => {

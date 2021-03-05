d5d7c976eee6fe042586b490604871c236f448d6
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-03-04 21:46:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
+get('/', (req, res) => {
 	res.send('Hello World')
 })
 
-app.listen(port, () => {
+listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
 

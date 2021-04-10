75ada295b930c3bef2b1e4314d2b5c47af0e32f3
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-04-09 19:38:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,3 +27,10 @@ function handleFiles(event) {
 }
 */
 
+const actualBtn = document.getElementById('actual-btn');
+
+const fileChosen = document.getElementById('file-chosen');
+
+actualBtn.addEventListener('change', function(){
+  fileChosen.textContent = this.files[0].name
+})

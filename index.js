33ea5448ac9d34ecf7e0ec880c38b6e85c2ad801
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-04-09 19:26:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -41,3 +41,13 @@ function SavePhoto(e)
     xhr.open("POST", '/upload/image'); 
     xhr.send(formData);
 }
+
+<input id="image-file" type="file" onchange="SavePhoto(this)" >
+<br><br>
+Choose file and open chrome console > network tab to see the request details.
+<br><br>
+<small>Because in this example we send request to https://stacksnippets.net/upload/image the response code will be 404 ofcourse...</small>
+
+<br><br>
+(the stack overflow snippets, has some problem with error handling - the xhr.status is zero (instead of 404) which is similar to situation when we run script from file on <a href="https://stackoverflow.com/a/10173639/860099">local disc</a> - so I provide also js fiddle version which shows proper http error code <a href="https://jsfiddle.net/Lamik/k6jtq3uh/2/">here</a>)
+

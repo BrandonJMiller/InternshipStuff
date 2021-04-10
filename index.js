c55cd57e6dd6bd7430e6735dd46b7433b006a0ce
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-04-09 19:35:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,3 @@ function handleFiles(event) {
 }
 */
 
-let photo = document.getElementById("image-file").files[0];
-let formData = new FormData();
-     
-formData.append("photo", photo);
-fetch('/upload/image', {method: "POST", body: formData});

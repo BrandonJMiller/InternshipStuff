3d9aceefb2f1e9d6a34d57bfca549ed35b73dd17
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-04-16 21:01:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,18 +19,3 @@ app.post('/submit-form', (req, res) => {
 	const username = req.body.username
 	res.end()
 })
-/*
-function handleFiles(event) {
-    var files = event.target.files;
-    $("#src").attr("src", URL.createObjectURL(files[0]));
-    document.getElementById("audio").load();
-}
-*/
-
-const actualBtn = document.getElementById('actual-btn');
-
-const fileChosen = document.getElementById('file-chosen');
-
-actualBtn.addEventListener('change', function(){
-  fileChosen.textContent = this.files[0].name
-})

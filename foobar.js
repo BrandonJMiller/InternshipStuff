97f506edc8cc31f678f12e9381ef9eb8976a61a5
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-04-23 10:42:12 -0700

    Update foobar.js

--- foobar.js
+++ foobar.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+//console.log("hello world")
+<!DOCTYPE html>
+<html>
+<body>
+    
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <script src="index.js"></script>
+
+    <button> Hello there! </button>
+    </body>
+</html>

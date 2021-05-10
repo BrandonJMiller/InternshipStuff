5bef26a9566013167f76a17fca704a694b5cf0f2
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-05-10 15:42:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const server = express();
+const document;
 const app = express()
 
 server.get('/', (req, res) => {

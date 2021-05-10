9a79f4838b6f71a0abfef166a1db5738e11d3355
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-05-09 17:02:56 -0700

    Update foobar.js

--- foobar.js
+++ foobar.js
@@ -1,38 +1,13 @@
-
-console.log("hello world")
 <!DOCTYPE html>
 <html>
 <body>
-    
-    <link href="style.css" rel="stylesheet" type="text/css" />
-    <script src="index.js"></script>
 
-    <button> Hello there! </button>
-    </body>
-</html>
+<p>Click on the "Choose File" button to upload a file:</p>
 
-/*
-<html>
-    <head> 
-        <script>
-            function setup() {
-                document.getElementById('buttonid').addEventListener('click', openDialog);
-                function openDialog() {
-                    document.getElementById('fileid').click();
-                }
-                document.getElementById('fileid').addEventListener('change', submitForm);
-                function submitForm() {
-                    document.getElementById('formid').submit();
-                }
-            }
-        </script> 
-    </head>
-    <body onload="setup()">
-        <form id='formid' action="form.php" method="POST" enctype="multipart/form-data"> 
-            <input id='fileid' type='file' name='filename' hidden/>
-            <input id='buttonid' type='button' value='Upload MB' /> 
-            <input type='submit' value='Submit' /> 
-        </form> 
-    </body> 
+<form action="/action_page.php">
+  <input type="file" id="myFile" name="filename">
+  <input type="submit">
+</form>
+
+</body>
 </html>
-*/

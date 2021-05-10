b4965feceffd8893c48cfd58317532c579005329
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-05-09 20:13:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const server = express();
-const realFileBtn = document.getElementById("real-file");
-const customBtn = document.getElementById("custom-button");
-const customTxt = document.getElementById("custom-text");
 const app = express()
 
 server.get('/', (req, res) => {
@@ -22,19 +19,3 @@ app.post('/submit-form', (req, res) => {
 	const username = req.body.username
 	res.end()
 })
-
-
-
-customBtn.addEventListener("click", function() {
-  realFileBtn.click();
-});
-
-realFileBtn.addEventListener("change", function() {
-  if (realFileBtn.value) {
-    customTxt.innerHTML = realFileBtn.value.match(
-      /[\/\\]([\w\d\s\.\-\(\)]+)$/
-    )[1];
-  } else {
-    customTxt.innerHTML = "No file chosen, yet.";
-  }
-});

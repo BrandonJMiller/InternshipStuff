11472bef33317eca54095fd455a34863c5678b3c
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-05-10 15:24:02 -0700

    Update foobar.js

--- foobar.js
+++ foobar.js
@@ -2,16 +2,9 @@
 <html>
 <body>
 
-<br>
-
-<!-- actual upload which is hidden -->
-<input type="file" id="actual-btn" hidden/>
-
-<!-- our custom upload button -->
-<label for="actual-btn">Choose File</label>
-
-<!-- name of file chosen -->
-<span id="file-chosen">No file chosen</span>
+<input type="file" id="real-file" hidden="hidden" />
+<button type="button" id="custom-button">CHOOSE A FILE</button>
+<span id="custom-text">No file chosen, yet.</span>
 
 </body>
 </html>

abb70109221cf812394ea6236f3652976d2811b2
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-05-18 22:37:21 -0700

    Update foobar.js

--- foobar.js
+++ foobar.js
@@ -1,42 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>File(s) size</title>
-</head>
+var http = require('http');
 
-<body>
-  <form name="uploadForm">
-    <div>
-      <input id="uploadInput" type="file" name="myFiles" multiple>
-      selected files: <span id="fileNum">0</span>;
-      total size: <span id="fileSize">0</span>
-    </div>
-    <div><input type="submit" value="Send file"></div>
-  </form>
-
-  <script>
-  function updateSize() {
-    let nBytes = 0,
-        oFiles = this.files,
-        nFiles = oFiles.length;
-    for (let nFileId = 0; nFileId < nFiles; nFileId++) {
-      nBytes += oFiles[nFileId].size;
-    }
-    let sOutput = nBytes + " bytes";
-    // optional code for multiples approximation
-    const aMultiples = ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
-    for (nMultiple = 0, nApprox = nBytes / 1024; nApprox > 1; nApprox /= 1024, nMultiple++) {
-      sOutput = nApprox.toFixed(3) + " " + aMultiples[nMultiple] + " (" + nBytes + " bytes)";
-    }
-    // end of optional code
-    document.getElementById("fileNum").innerHTML = nFiles;
-    document.getElementById("fileSize").innerHTML = sOutput;
-  }
-
-  document.getElementById("uploadInput").addEventListener("change", updateSize, false);
-  </script>
-</body>
-</html>
-
-
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+  res.write('<input type="file" name="filetoupload"><br>');
+  res.write('<input type="submit">');
+  res.write('</form>');
+  return res.end();
+}).listen(5000);

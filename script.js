b6158836c64f478d359db4f9384aff02448c9835
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-05-20 11:54:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const path = require('path')
-const multer = require('multer');
+var multer = require('multer');
 const PORT = process.env.PORT || 5000
 const server = express();
 const app = express()

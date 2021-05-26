9264cd0b854885da3e4860155f47dd493c449697
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-05-25 22:13:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const server = express();
 const app = express()
 
 var http = require('http');
-//var formidable = require('formidable');
+var formidable = require('formidable');
 var fs = require('fs');
 
 http.createServer(function (req, res) {

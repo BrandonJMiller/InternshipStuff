18f8c88232c2c1b96b52b004103e0ae49f60bda5
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-05-26 14:37:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,39 @@ const PORT = process.env.PORT || 5000
 const server = express();
 const app = express()
 
+var multer = require('multer');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+ var Storage = multer.diskStorage({
+     destination: function(req, file, callback) {
+         callback(null, "./Images");
+     },
+     filename: function(req, file, callback) {
+         callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+     }
+ });
+
+var upload = multer({
+     storage: Storage
+ }).array("imgUploader", 3); //Field name and max count
+
+array(fieldname[, maxCount])
+ app.get("/", function(req, res) {
+     res.sendFile(__dirname + "/index.html");
+ });
+ app.post("/api/Upload", function(req, res) {
+     upload(req, res, function(err) {
+         if (err) {
+             return res.end("Something went wrong!");
+         }
+         return res.end("File uploaded sucessfully!.");
+     });
+ });
+
+app.listen(5000, function(a) {
+     console.log("Listening to port 2000");
+ });
 /*
 var http = require('http');
 var formidable = require('formidable');

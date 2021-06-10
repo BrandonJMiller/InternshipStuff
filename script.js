da0e6a1a55044db76a7de3d714c6896d88eacb7c
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-06-10 13:08:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,66 +4,6 @@ const PORT = process.env.PORT || 5000
 const server = express();
 const app = express()
 
-/*
-const fileUpload = require('express-fileupload');
-
-// default options
-app.use(fileUpload());
-
-app.post('/upload', function(req, res) {
-  let sampleFile;
-  let uploadPath;
-
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
-
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  sampleFile = req.files.sampleFile;
-  uploadPath = __dirname + '/somewhere/on/your/server/' + sampleFile.name;
-
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(uploadPath, function(err) {
-    if (err)
-      return res.status(500).send(err);
-
-    res.send('File uploaded!');
-  });
-});
-*/
-
-
-var http = require('http');
-var formidable = require('formidable')
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
-});
-
- app.get("/", function(req, res) {
-     res.sendFile(__dirname + "/index.html");
- });
-
-
 server.get('/', (req, res) => {
 	res.send('Hello World')
 })

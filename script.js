ea80c9e578de3989a0a79592cbcb9beb799696db
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-06-10 17:42:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,31 +5,21 @@ const server = express();
 const app = express()
 
 
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
+const formidable = require('formidable');
+const upload = formidable({dest: __dirname + '/uploads/images'});
 
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
-});//.listen(5000)
+app.use(express.static('public'));
+
+app.post('/upload', upload.single('photo'), (req, res) => {
+    if(req.file) {
+        res.json(req.file);
+    }
+    else throw 'error';
+});
+
+app.listen(PORT, () => {
+    console.log('Listening at ' + PORT );
+});
 
 
 server.get('/', (req, res) => {

7efa1f34728b5087b8adbec5adb9c2a1ab1504c4
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-06-11 09:04:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+//const PORT = process.env.PORT || 5000
 const server = express();
-const app = express()
+//const app = express()
 
 
+var formidable = require('formidable');
+
+var app = express();
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res){
+    var form = new formidable.IncomingForm();
+
+    form.parse(req);
+
+    form.on('fileBegin', function (name, file){
+        file.path = __dirname + '/uploads/' + file.name;
+    });
+
+    form.on('file', function (name, file){
+        console.log('Uploaded ' + file.name);
+    });
+
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000);
 
 
-
+/*
 server.get('/', (req, res) => {
 	res.send('Hello World')
 })
@@ -17,6 +42,6 @@ server.listen(PORT, () => {
 app.use(express.urlencoded({
 	extended: true
 }))
-
+*/
 
 

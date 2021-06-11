1ab5f9fb947d207b7acc57a6c465de347d9b9aa6
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-06-10 17:15:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const server = express();
-const app = express()
+//const app = express()
 
 
 var formidable = require('formidable');
-
-//var app = express();
+var app = express();
 
 app.get('/', function (req, res){
     res.sendFile(__dirname + '/index.html');

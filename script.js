48d36f16d1f403750a930c98ec923f89003acd97
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-06-18 23:19:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,34 +5,6 @@ const server = express();
 var formidable = require('formidable');
 var app = express();
 
-var http = require('http');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
-}).listen(5000);
-
-
-
-/*
 app.get('/', function (req, res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -57,8 +29,6 @@ app.post('/', function (req, res){
 app.listen(PORT,() => {
 	console.log('listening on ${PORT}')
 })
-*/
-
 
 /*
 server.get('/', (req, res) => {

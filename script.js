2198024192f146f6088884d0e457aacb8e0b892d
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-07-15 14:32:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,47 +5,6 @@ const server = express();
 var formidable = require('formidable');
 var app = express();
 
-
-
-
-function previewImages() {
-
-  
-  
-  if (this.files) {
-    [].forEach.call(this.files, readAndPreview);
-  }
-
-  function readAndPreview(file) {
-
-    // Make sure `file.name` matches our extensions criteria
-    if (!/\.(jpe?g|png|gif)$/i.test(file.name)) {
-      return alert(file.name + " is not an image");
-    } // else...
-    
-    var reader = new FileReader();
-    
-    reader.addEventListener("load", function() {
-      var image = new Image();
-      image.height = 100;
-      image.title  = file.name;
-      image.src    = this.result;
-      
-    });
-    
-    reader.readAsDataURL(file);
-    
-  }
-
-}
-
-document.querySelector('#file-input').addEventListener("change", previewImages);
-
-
-
-
-
-
 app.get('/', function (req, res){
     res.sendFile(__dirname + '/index.html');
 });

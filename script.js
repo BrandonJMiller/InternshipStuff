967c84d4024f4f169f06e00941b5c6263c0eac2a
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-07-26 11:37:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -900,9 +900,9 @@ var app = express();
 
 
 
-//app.get('/', function (req, res){
+app.get('/', function (req, res){
 //    res.sendFile(__dirname + '/index.html');
-//});
+});
 
 app.listen(5000);
 /*
